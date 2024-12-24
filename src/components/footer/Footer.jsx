c92fe2703b2015ceab4200a5ef041d1163f6955d
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-12-24 16:33:51 +0530

    hii

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -37,6 +37,8 @@ import xWhite from '@public/Images/footer/x-white.png';
 import Instagram from '@public/Images/footer/instagram-white.png'
 import Linkedin from '@public/Images/footer/linkedin-white.png'
 
+import Link from "next/link";
+
 
 export const Footer = () => {
   return (
@@ -44,10 +46,10 @@ export const Footer = () => {
         <div className='footer-container'>
           <div className='footer-left'>
             <div className='footer-contents'>
-                <p>Terms of Service</p>
-                <p>Privacy Policy</p>
-                <p>Cookie Policy</p>
-                <p>Refund Policy</p>
+            <div className="footer-content-text"><Link href="/terms-of-service"> <p>Terms of Service</p> </Link></div>
+            <div className="footer-content-text"><Link href="/privacy-policy"><p>Privacy Policy</p></Link></div>
+            <div className="footer-content-text"><Link href="/cookie-policy"><p>Cookie Policy</p></Link></div>
+            <div className="footer-content-text"><Link href="/refund-policy"><p>Refund Policy</p></Link></div>
             </div>
             <div className='footer-contents'>
             <Image src={logo} alt='prfec logo'/>
